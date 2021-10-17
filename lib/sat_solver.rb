3f7afd9d0b9bffe43585a8bffc92a8a55af403ca
Justin MacCarthy <justin@maccarthy.org>
2021-10-17 13:37:29 +0700

    Small tidy up and change the formatter in rspec

--- lib/sat_solver.rb
+++ lib/sat_solver.rb
@@ -1,49 +1,49 @@
 class SatSolver
-    def initialize()
-      @tokens =[]
-      @var_map ={}
-      @counter = 1
-    end
+  def initialize
+    @tokens = []
+    @var_map = {}
+    @counter = 1
+  end
 
+  def solve
+    Solution.new
+  end
 
-    def solve 
-      Solution.new
-    end 
+  def <<(token)
+    @tokens << token
+    self
+  end
 
-    def <<(token)
-      @tokens << token
-      self
-    end 
+  def new_var
+    name = new_var_name
+    bnf = BNF.new(name)
+    @var_map[name.to_sym] = bnf
+    bnf
+  end
 
-    def new_var
-      name = new_var_name
-      bnf = BNF.new(name)
-      @var_map[name.to_sym] = bnf
-      bnf
-    end
-
-    def new_var_name
-      @counter += 1
-      "var_#{@counter}"
-    end
+  def new_var_name
+    @counter += 1
+    "var_#{@counter}"
+  end 
 
   class Solution
     def satisfied?
-      true 
-    end 
-  end 
+      true
+    end
+  end
 
   class BNF
     def initialize(name)
-      @name 
+      @name = nil
       @state = true
     end
 
     def -@
       @state = false
-    end 
+    end
 
-
-  end 
-    
+    def +@
+      @state = true
+    end
+  end
 end
--- spec/sat_solver_spec.rb
+++ spec/sat_solver_spec.rb
@@ -2,25 +2,41 @@ require 'spec_helper'
 require_relative '../lib/sat_solver'
 
 describe SatSolver do
-
-  context "solving simple small CNF problems" do 
-      it "solves CNF to solver: (a or b) and (not a or b) and (a or not b) " do 
-        solver = SatSolver.new
-        a = solver.new_var
-        b = solver.new_var
-     
-        solver << [a,b] 
-        solver << [-a,b] 
-        solver << [a,-b]
-
-        result = solver.solve
-        expect(result).to be_kind_of(Object)
-        expect(result).to respond_to(:satisfied?)
-
-        expect(result.satisfied?).to be_instance_of(TrueClass)
+  context 'solving simple small CNF problems' do
+    it 'solves  ' do
+      solver = SatSolver.new
     end 
 
+    it "creates new_variable for the solver" do 
+      
+      solver = SatSolver.new
+      a = solver.new_var
+      b = solver.new_var
+      expect(a).to be_kind_of(Object)
+      expect(b).to be_kind_of(Object)
+    end 
 
-  end 
+    it "it acccept a CNF to solver: (a or b) and (not a or b) and (a or not b)" do 
+      solver = SatSolver.new
+      a = solver.new_var
+      b = solver.new_var
+      solver << [a, b]
+      solver << [-a, b]
+      solver << [a, -b]
+    end 
 
-end 
+    it "returns a solution" do 
+      solver = SatSolver.new
+      a = solver.new_var
+      b = solver.new_var
+      solver << [a, b]
+      solver << [-a, b]
+      solver << [a, -b]
+      result = solver.solve
+      expect(result).to be_kind_of(Object)
+      expect(result).to respond_to(:satisfied?)
+      expect(result.satisfied?).to be_instance_of(TrueClass)
+    end
+
+  end
+end
